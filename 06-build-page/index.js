05d1af1648a29fbc62a147b3c36c884a92ca8cbe
Kirill Kostin <konmedoed@gmail.com>
2024-01-25 01:46:56 +0300

    fix: remove repeated files

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,104 +1,127 @@
 const fs = require ('fs');
 const path = require ('path');
 
-fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {
-  if (err) console.log('folder exist!');
-})
+fs.rm(path.join(__dirname, 'project-dist', 'assets'), { recursive: true, force: true }, (err) => {
+  //if (err) throw err;
+  build();
+});
 
-const componentFiles = [];
-let arrLayoutTemplate = [];
-
-fs.readdir(path.join(__dirname, 'components'), (err, files) => {
-  if (err) console.log(err);
-  files.forEach(item => componentFiles.push(item));
-  readTemplate();
-})
-
-function readTemplate(){
-  fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
+function build(){
+  fs.mkdir(path.join(__dirname, 'project-dist'), (err) => {
+    if (err) console.log('folder exist!');
+  });
+  
+  const componentFiles = [];
+  let arrLayoutTemplate = [];
+  
+  fs.readdir(path.join(__dirname, 'components'), (err, files) => {
     if (err) console.log(err);
-    arrLayoutTemplate = data.toString().split(`\r\n`);
-    const components = [];
-    arrLayoutTemplate.forEach(item => {
-      if (item.includes('{{') && item.includes('}}')) {
-        const array = item.split('{{').filter(item => item.includes('}}'));
-        const namesOfComp = array.map(item => {
-          const index = item.indexOf('}');
-          return item.slice(0, index);
-        });
-        namesOfComp.forEach(item => components.push(item));
-      }
-    })
-    getLayout(components);
+    files.forEach(item => {
+      const address = path.join(__dirname, 'components', item);
+      if (path.extname(address) === '.html') componentFiles.push(path.basename(address, path.extname(item)));
+    });
+    readTemplate();
   })
-}
-
-function getLayout(arr){
-  const layDataObj = {};
-  arr.forEach((item, index) => {
-    const readLay = fs.createReadStream(path.join(__dirname, 'components', (item + '.html')), 'utf-8');
-    let data = '';
-    let counter = index;
-    readLay.on('data', (chunk) => data += chunk);
-    readLay.on('end', () => {
-      layDataObj[item] = data;
-      if (counter === arr.length-1) generateUnionLayout(layDataObj);
+  
+  function readTemplate(){
+    fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
+      if (err) console.log(err);
+      arrLayoutTemplate = data.toString().split(`\r\n`);
+      getLayout(componentFiles);
+    })
+  }
+  
+  function getLayout(arr){
+    const layDataObj = {};
+    arr.forEach((item, index) => {
+      const readLay = fs.createReadStream(path.join(__dirname, 'components', (item + '.html')), 'utf-8');
+      let data = '';
+      let counter = index;
+      readLay.on('data', (chunk) => data += chunk);
+      readLay.on('end', () => {
+        layDataObj[item] = data;
+        if (counter === arr.length-1) generateUnionLayout(layDataObj);
+      });
+    })
+  }
+  
+  function generateUnionLayout(data){
+    let newTemplate = arrLayoutTemplate.join('\r\n');
+    for (let key in data) {
+      const strKey = '{{' + key + '}}';
+      if (newTemplate.includes(strKey)){
+        const correctedTemplate = newTemplate.replace(strKey, data[key]);
+        newTemplate = correctedTemplate;
+      }
+    }
+    fs.writeFile(
+      path.join(__dirname, 'project-dist', 'index.html'),
+      newTemplate,
+      (err) => {if (err) throw err}
+    )
+  }
+  
+  //Copy styles
+  
+  fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
+    if (err) throw err;
+  })
+  
+  fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
+    if (err) throw err;
+    let arr = [];
+    const styleFiles = files.filter(item => {
+      if (path.extname(item) === '.css') return true;
+    })
+    styleFiles.forEach((item, index) => {
+        let style = '';
+        let stream = fs.createReadStream(path.join(__dirname, 'styles', item), 'utf-8');
+        stream.on('data', (chunk) => {
+          style += chunk;
+        });
+        stream.on('end', () => {
+          arr[index] = style;
+          if (index === (styleFiles.length - 1)) writeStyles(arr);
+        });
     });
   })
-}
-
-function generateUnionLayout(data){
-  let newTemplate = arrLayoutTemplate.join('\r\n');
-  for (let key in data) {
-    const strKey = '{{' + key + '}}';
-    if (newTemplate.includes(strKey)){
-      const correctedTemplate = newTemplate.replace(strKey, data[key]);
-      newTemplate = correctedTemplate;
-    }
+  
+  function writeStyles(data){
+    const styleStr = data.join('');
+    const stream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
+    stream.write(styleStr);
   }
-  fs.writeFile(
-    path.join(__dirname, 'project-dist', 'index.html'),
-    newTemplate,
-    (err) => {if (err) throw err}
-  )
-}
-
-//Copy styles
-
-fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), '', (err) => {
-  if (err) throw err;
-})
-
-fs.readdir(path.join(__dirname, 'styles'), (err, files) => {
-  if (err) throw err;
-  let arr = [];
-  const styleFiles = files.filter(item => {
-    if (path.extname(item) === '.css') return true;
+  
+  //Copy assets
+  
+  fs.mkdir(path.join(__dirname, 'project-dist', 'assets'), (err) => {
+    if (err) console.log('folder exist!');
+    copyFile(path.join(__dirname, 'assets'), '');
   })
-  styleFiles.forEach((item, index) => {
-      let style = '';
-      let stream = fs.createReadStream(path.join(__dirname, 'styles', item), 'utf-8');
-      stream.on('data', (chunk) => {
-        style += chunk;
-      });
-      stream.on('end', () => {
-        arr[index] = style;
-        if (index === (styleFiles.length - 1)) writeStyles(arr);
-      });
-  });
-})
-
-function writeStyles(data){
-  const styleStr = data.join('');
-  const stream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'style.css'));
-  stream.write(styleStr);
+  
+  function copyFile(src, subSrc){
+    fs.readdir(src, { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+      files.forEach(item => {
+        if (item.isDirectory()) {
+          copyFile(path.join(__dirname, 'assets', path.join(subSrc, item.name)), path.join(subSrc, item.name));
+          fs.mkdir(path.join(__dirname, 'project-dist', 'assets', path.join(subSrc, item.name)), (err) => {
+            if (err) console.log('folder exist!');
+          })
+        }
+        else {
+          fs.readFile(path.join(src, item.name), (err, data) => {
+            if (err) throw err;
+            makeFile(subSrc, item.name, data);
+          })
+        }
+      })
+    })
+  }
+  
+  function makeFile(subSrc, fileName, data){
+    fs.writeFile(path.join(__dirname, 'project-dist', 'assets', subSrc, fileName), data, (err) => {
+      if (err) throw err;
+    })
+  }
 }
-
-//Copy assets
-
-fs.cp(
-  path.join(__dirname, 'assets'),
-  path.join(__dirname, 'project-dist', 'assets'),
-  {recursive: true},
-  (err) => {if (err) throw err}
-)
